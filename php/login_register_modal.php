46b9b746a93653f69799b2a97d4be425c4fb8935
iNSDX <lrp_ruspeg@hotmail.com>
2017-04-20 03:44:37 +0200

    Cambio carpetas

---
+++ php/login_register_modal.php
@@ -0,0 +1,68 @@
+    <div class="container">
+		 <div class="modal fade login" id="loginModal">
+		      <div class="modal-dialog login animated">
+    		      <div class="modal-content">
+    		         <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title">Login with</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="box">
+                             <div class="content">
+                                <div class="social">
+                                    <a class="circle github" href="/auth/github">
+                                        <i class="fa fa-github fa-fw"></i>
+                                    </a>
+                                    <a id="google_login" class="circle google" href="/auth/google_oauth2">
+                                        <i class="fa fa-google-plus fa-fw"></i>
+                                    </a>
+                                    <a id="facebook_login" class="circle facebook" href="/auth/facebook">
+                                        <i class="fa fa-facebook fa-fw"></i>
+                                    </a>
+                                </div>
+                                <div class="division">
+                                    <div class="line l"></div>
+                                      <span>or</span>
+                                    <div class="line r"></div>
+                                </div>
+                                <div class="error"></div>
+                                <div class="form loginBox">
+                                    <form method="post" action="/login" accept-charset="UTF-8">
+                                    <input id="email" class="form-control" type="text" placeholder="Email" name="email">
+                                    <input id="password" class="form-control" type="password" placeholder="Password" name="password">
+                                    <input class="btn btn-default btn-login" type="button" value="Login" onclick="loginAjax()">
+                                    </form>
+                                </div>
+                             </div>
+                        </div>
+                        <div class="box">
+                            <div class="content registerBox" style="display:none;">
+                             <div class="form">
+                                <form method="post" html="{:multipart=>true}" data-remote="true" action="procesar_nuevo_usuario.php" accept-charset="UTF-8"/>
+                                <input id="nombre" name="nombre" placeholder="Nombre" class="form-control" type="text" value="<?php echo $formulario['nombre'];?>" required/>
+                                <input id="apellidos" name="apellidos" placeholder="Apellidos" class="form-control" type="text" value="<?php echo $formulario['apellidos'];?>" required/>
+                                <input id="fechaNacimiento" name="fechaNacimiento" class="form-control" type="date" value="<?php echo $formulario['fechaNacimiento'];?>" required/>
+                                <input id="email" name="email" placeholder="Email" class="form-control" type="text" value="<?php echo $formulario['email'];?>" required/>
+                                <input id="password" name="password" placeholder="Password" class="form-control" type="password" required/>
+                                <input id="password_confirmation" name="password_confirmation" placeholder="Repeat Password" class="form-control" type="password" required/>
+                                <input class="btn btn-default btn-register" name="commit" type="submit" value="Create account"/>
+                                </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="forgot login-footer">
+                            <span>Looking to
+                                 <a href="javascript: showRegisterForm();">create an account</a>
+                            ?</span>
+                        </div>
+                        <div class="forgot register-footer" style="display:none">
+                             <span>Already have an account?</span>
+                             <a href="javascript: showLoginForm();">Login</a>
+                        </div>
+                    </div>
+    		      </div>
+		      </div>
+		  </div>
+    </div>
