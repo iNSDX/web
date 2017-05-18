68c25d54b777d4a63832341914393b9cbac3ebe9
juanmiguelruiz <juanmiguelruizpazos@gmail.com>
2017-05-18 11:38:27 +0200

    Empiezo parte interna
    empleado ok!

--- Empleado/index.php
+++ Empleado/index.php
@@ -1,27 +1,47 @@
-<?php
-    session_start();
+<!DOCTYPE HTML>
 
-    if(isset($_SESSION['usuario'])){
-        if($_SESSION['usuario']['TIPO'] != 'Empleado'){
-            header('Location: ../');
-        }
-    }else{
-        header('Location: ../');
-    }
- ?>
+<html>
+	<head>
+		<title>Gestión Estanco</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Bienvenido <?php echo $_SESSION['usuario']['NOMBRE'] ?> </h1>
+		<?php include_once("includes/links.php") ?>
+	</head>
+	<body>
 
-    <a href="logout.php">Cerrar sesión</a>
+		<!-- Wrapper -->
+			<div id="wrapper">
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+							<!-- Header -->
+								<header id="header">
+									<a href="index.html" class="logo"><strong>Dependiente</strong> NOMBRE </a>
+								</header>
 
-</body>
+							<!-- Banner -->
+								<section id="banner">
+									<div class="content">
+										<header>
+											<h1>Hola, NOMBRE<br /></h1>
+											<p>Esta es la interfaz de dependiente.</p>
+										</header>
+										<p>Con esta interfaz usted podrá hacer una nueva venta, acceder al historial de las mismas, hacer una factura, acceder al historial de las mismas, realizar el arqueo de caja, así como acceder al inventario.</p>
+										<ul class="actions">
+											<li><a href="nVenta.php" class="button big">Nueva Venta</a></li>
+										</ul>
+									</div>
+									<span class="image object">
+									</span>
+								</section>
+
+						</div>
+					</div>
+
+					<?php include_once("includes/sidebar.php") ?>
+
+			</div>
+
+	</body>
 </html>
